1fe1a0df47a8014b150c9a2936038523db64e276
hikaro.cordeiro@gmail.com <xinforinfola13>
2016-06-17 13:57:00 -0300

    teste heroku

--- teste.js
+++ teste.js
@@ -1,10 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var port = process.env.PORT || 8080
 
 app.get('/', function(req, res){
     res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
     console.log('listening on *:3000');
 });
