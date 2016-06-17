d3a385c88946c8e814891db094ee126513161ab5
hikaro.cordeiro@gmail.com <xinforinfola13>
2016-06-17 13:49:37 -0300

    teste heroku

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = env.PORT || 3000;
+var port = 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
