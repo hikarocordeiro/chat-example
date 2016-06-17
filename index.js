5548e3f4de6cccf1e826ed30d6812e6353ee4865
hikaro.cordeiro@gmail.com <xinforinfola13>
2016-06-17 15:50:01 -0300

    rolagem de d6

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
