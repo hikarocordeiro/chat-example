1385bad4806a830fe40a09829478ca4ea61ae877
hikaro.cordeiro@gmail.com <xinforinfola13>
2016-06-20 13:25:26 -0300

    rolagem de d6

--- index.js
+++ index.js
@@ -7,15 +7,24 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 
   socket.on('roll message', function(msg){
-    var dice = 1;
+    var dice = 0;
+
     if(msg == 'd6'){
-      dice = Math.floor(Math.random() * (7 - 1)) + 1;
+      var dice = getRandomInt(1, 6);
     }
     io.emit('chat message', '<i><img src="https://db.tt/Upbap5C6" class="d6-dice-icon"/></i> = '+dice);
   });
